08cf777d6f3a453ee4ffd03716edda7093c2d5a4
Llanillo <llanillo4@hotmail.com>
2023-11-27 09:26:38 -0300

    Agrega swagger

--- src/main/java/com/dim/Controller/LugaresApi.java
+++ src/main/java/com/dim/Controller/LugaresApi.java
@@ -1,8 +1,8 @@
 package com.dim.Controller;
 
 
-import com.dim.Module.Coordenada;
 import com.dim.Module.Lugares;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +18,7 @@ public interface LugaresApi {
 
     //NOMBRE, PADRON, DIRECCION, FECHA DE ALTA, ESTADO, [LATITUD Y LONGITUD]
 
-
-
+    @Operation(summary = "Busca latitud y longitud", description = "Retorna los lugares de esas coordenadas")
     @GetMapping(value = "/{latitud}/{longitud}", produces = {"application/json", "application/vnd.api+json"})
     @ResponseStatus(HttpStatus.OK)
     ResponseEntity<Collection<Lugares>> getLugaresByCoordenadas(
